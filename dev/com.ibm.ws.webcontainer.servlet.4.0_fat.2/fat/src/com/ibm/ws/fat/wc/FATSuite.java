a46c20d07eaec54c601a611f81faa3d27ced96cb
Phu Dinh <pmdinh@us.ibm.com>
2026-02-01 15:50:55 -0500

    FAT tests

--- dev/com.ibm.ws.webcontainer.servlet.4.0_fat.2/fat/src/com/ibm/ws/fat/wc/FATSuite.java
+++ dev/com.ibm.ws.webcontainer.servlet.4.0_fat.2/fat/src/com/ibm/ws/fat/wc/FATSuite.java
@@ -19,6 +19,7 @@ import com.ibm.ws.fat.util.FatLogHandler;
 import com.ibm.ws.fat.wc.tests.WCPartitionedAttributeTests;
 import com.ibm.ws.fat.wc.tests.WCPartitionedCookieAttributeSecurityTest;
 import com.ibm.ws.fat.wc.tests.WCRequestAutoDecompressTest;
+import com.ibm.ws.fat.wc.tests.WCResponseContentLenghtChunkedEncodingTest;
 import com.ibm.ws.fat.wc.tests.WCResponseHeadersTest;
 import com.ibm.ws.fat.wc.tests.WCSameSiteCookieAttributeSecurityTest;
 import com.ibm.ws.fat.wc.tests.WCSameSiteCookieAttributeTests;
@@ -49,6 +50,7 @@ import componenttest.rules.repeater.RepeatTests;
 @RunWith(Suite.class)
 @SuiteClasses({
                 WCRequestAutoDecompressTest.class,
+                WCResponseContentLenghtChunkedEncodingTest.class,
                 WCResponseHeadersTest.class,
                 WCSameSiteCookieAttributeTests.class,
                 WCSameSiteCookieAttributeSecurityTest.class,
