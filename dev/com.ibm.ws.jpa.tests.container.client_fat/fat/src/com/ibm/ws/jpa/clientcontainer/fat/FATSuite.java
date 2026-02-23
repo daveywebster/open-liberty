d0a6f7cb8a0d165a9396c481998f6be9ec651318
Paul Nicolucci <pnicolucci@gmail.com>
2026-02-23 14:15:18 -0500

    Repeat for Jakarta EE11

--- dev/com.ibm.ws.jpa.tests.container.client_fat/fat/src/com/ibm/ws/jpa/clientcontainer/fat/FATSuite.java
+++ dev/com.ibm.ws.jpa.tests.container.client_fat/fat/src/com/ibm/ws/jpa/clientcontainer/fat/FATSuite.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2024 IBM Corporation and others.
+ * Copyright (c) 2021, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
- * SPDX-License-Identifier: EPL-2.0
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 
 package com.ibm.ws.jpa.clientcontainer.fat;
@@ -38,7 +35,7 @@ public class FATSuite {
     @ClassRule
     public static RepeatTests r = RepeatTests.with(FeatureReplacementAction.NO_REPLACEMENT().fullFATOnly())
                     .andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
-                    .andWith(FeatureReplacementAction.EE10_FEATURES())
+                    .andWith(FeatureReplacementAction.EE10_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
                     .andWith(FeatureReplacementAction.EE11_FEATURES());
 //    @ClassRule
 //    public static ExternalResource testRule = new ExternalResource() {
