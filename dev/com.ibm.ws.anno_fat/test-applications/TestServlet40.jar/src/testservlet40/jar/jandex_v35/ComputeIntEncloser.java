5beab5465c8c0cb0cbb8aa32709085a57ed602a6
Benjamin Confino <benjamic@uk.ibm.com>
2026-02-24 18:01:54 +0000

    update copyrights

--- dev/com.ibm.ws.anno_fat/test-applications/TestServlet40.jar/src/testservlet40/jar/jandex_v35/ComputeIntEncloser.java
+++ dev/com.ibm.ws.anno_fat/test-applications/TestServlet40.jar/src/testservlet40/jar/jandex_v35/ComputeIntEncloser.java
@@ -1,4 +1,14 @@
-package testservlet40.jar.jandex_v35;
+/*******************************************************************************
+ * Copyright 2026 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ *******************************************************************************/
+ package testservlet40.jar.jandex_v35;
 
 public class ComputeIntEncloser {
 
